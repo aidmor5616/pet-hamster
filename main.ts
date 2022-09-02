f26c5841e8cd9b70226de3916d1a000f8bfe0f95
aidmor5616 <112516505+aidmor5616@users.noreply.github.com>
2022-09-01 19:06:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,20 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    sleep += 3
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        . # # . .
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    health += -10
 })
 let health = 10
+let sleep = 20
 basic.forever(function () {
     basic.showLeds(`
         . # # . .
@@ -60,8 +71,21 @@ basic.forever(function () {
         basic.pause(2000)
         basic.showString("you ugly")
     }
+    if (sleep == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # # .
+            . . # # .
+            # # # # #
+            `)
+    }
 })
 basic.forever(function () {
     health += -1
     basic.pause(2000)
+})
+basic.forever(function () {
+    sleep += -2
+    basic.pause(1000)
 })
